53e4b7a118a20f936ff0f8cd41ece976736f8285
rezmicrosoft <70175798+rezmicrosoft@users.noreply.github.com>
2021-06-07 15:08:05 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,9 @@
+while true; do
+	res=$(curl -s https://raw.githubusercontent.com/rezmicrosoft/drilltest/main/test)
+	if [ $res = 'true' ]; then
+		echo "got true"
+	else
+		echo "got false"
+	fi
+	sleep 5s
+done
