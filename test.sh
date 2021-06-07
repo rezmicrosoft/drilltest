422c286b58f7fa7d0c61d726f40b6a6d90804714
rezmicrosoft <70175798+rezmicrosoft@users.noreply.github.com>
2021-06-07 15:29:04 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@ while true; do
 	res=$(curl -s https://raw.githubusercontent.com/rezmicrosoft/drilltest/main/test)
 	if [ $res = 'true' ]; then
 		echo "got true"
+		exit 0
 	else
 		echo "got false"
 	fi
